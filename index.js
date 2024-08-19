36d1ca3683ecfb026229df6548858a792033048d
abdigunasetiawan <abdigunasetiawan0@gmail.com>
2024-08-19 23:46:09 +0700

    memperbaiki animasi navbar

--- index.js
+++ index.js
@@ -1,11 +1,9 @@
 const navbar = document.querySelector("nav");
 const hamburgerToggle = document.querySelector("#hamburger-toggle");
-const linksWrapper = document.querySelector("#links-wrapper");
 
 hamburgerToggle.addEventListener("click", () => {
   navbar.classList.toggle("active");
   hamburgerToggle.classList.toggle("active");
-  linksWrapper.classList.toggle("active");
 });
 let slidesPerView = 1;
 

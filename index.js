99be4951dd932dab148c3b94f657d225d8244dd3
abdigunasetiawan <abdigunasetiawan0@gmail.com>
2024-09-10 22:20:48 +0700

    optimisasi gambar

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ window.addEventListener("DOMContentLoaded", () => {
     events.map((event) => {
       eventSwiperWrapper.innerHTML += `<div class="swiper-slide ">
   <a href="" class="card__event group relative mx-auto block aspect-[2/3] w-[256px] overflow-hidden rounded-lg shadow-xl xl:w-[300px]">
-    <img class="h-full w-full object-cover transition-transform duration-300 group-hover:scale-110" src="${event.img_path}" alt="" />
+    <img loading="lazy" class="h-full w-full object-cover transition-transform duration-300 group-hover:scale-110" src="${event.img_path}" alt="" />
 
     <div class="absolute bottom-5 left-2.5 z-50 w-[calc(100%-20px)] lg:left-4 lg:w-[calc(100%-32px)]">
       <p class="max-w-max rounded-lg bg-[#D9D9D990] px-2 py-1 text-xs uppercase text-white">${event.type}</p>
@@ -36,7 +36,7 @@ window.addEventListener("DOMContentLoaded", () => {
     projects.map((project) => {
       projectSwiperWrapper.innerHTML += ` <div class="swiper-slide">
               <a href="${project.url}" target="_blank" class="card__project group relative mx-auto block aspect-video w-full overflow-hidden rounded-lg shadow-md">
-                <img class="h-full w-full object-cover transition-transform duration-300 group-hover:scale-110" src="${project.img_path}" alt="" />
+                <img loading="lazy" class="h-full w-full object-cover transition-transform duration-300 group-hover:scale-110" src="${project.img_path}" alt="" />
 
                 <div class="absolute bottom-5 left-2.5 z-50 w-[calc(100%-20px)] lg:left-4 lg:w-[calc(100%-32px)]">
                   <p class="max-w-max rounded-lg bg-[#D9D9D990] px-2 py-1 text-xs uppercase text-white">${project.type}</p>
